64ca6b9c934d65e886921f2338e2fa16144814c0
xlcoder <xlcoder166@gmail.com>
2016-10-08 18:16:24 +0800

    8/10 13Pomodoer 1. [Change] Modify Archive Display

--- usr/themes/default/archive.php
+++ usr/themes/default/archive.php
@@ -19,7 +19,8 @@
                     <li itemprop="interactionCount"><a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
     			</ul>
                 <div class="post-content" itemprop="articleBody">
-        			<?php $this->content('- 阅读剩余部分 -'); ?>
+                <!-- <?php $this->content('- 阅读剩余部分 -'); ?> </div> -->
+                <?php $this->summary(); ?><p><a href="<?php $this->permalink() ?>">展开阅读</a></p>
                 </div>
     		</article>
     	<?php endwhile; ?>
